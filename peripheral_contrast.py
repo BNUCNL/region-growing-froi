220216e16c7f6cf6a9fb16777d8251648cf633a7
liuzhaoguo <liu_zhaoguo@126.com>
2014-04-18 21:34:49 +0800

    renew the average contrast and data path

--- peripheral_contrast.py
+++ peripheral_contrast.py
@@ -6,64 +6,26 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-image_index = ["/nfs/t2/fmricenter/volume/S0001/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0004/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0005/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0006/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0007/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0009/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0010/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0012/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0014/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0016/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0020/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0025/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0027/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0034/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0035/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0036/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0037/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0038/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0040/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0041/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0051/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0053/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0057/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0059/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0068/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0069/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0070/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0074/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0076/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0077/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0081/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0085/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0087/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0091/obj.gfeat/cope1.feat/stats/tstat1.nii.gz",\
-             "/nfs/t2/fmricenter/volume/S0092/obj.gfeat/cope1.feat/stats/tstat1.nii.gz"]
+image_index = ["data/S1/tstat1.nii.gz","data/S2/tstat1.nii.gz",\
+               "data/S3/tstat1.nii.gz","data/S4/tstat1.nii.gz",\
+               "data/S5/tstat1.nii.gz"]
+seeds=[(26,38,25),(24,37,24),(24,37,25),(24,38,24),(23,38,26)]
 
-seeds=[(25,41,25),(25,36,29),(26,31,29),(23,38,23),(66,38,29),(25,39,24),(25,38,25),\
-       (25,41,26),(24,35,22),(22,35,24),(27,29,28),(68,36,24),(27,30,26),(23,37,24),\
-       (26,38,25),(24,38,27),(22,31,26),(23,34,26),(24,37,24),(24,37,25),(22,38,27),\
-       (23,40,27),(25,40,24),(24,38,24),(21,38,24),(24,29,28),(26,40,26),(24,39,26),\
-       (23,38,26),(21,40,22),(23,38,26),(23,39,23),(25,35,25),(23,38,30),(65,38,24)]
+output=['S1(26,38,25)','S2(24,37,24)','S3(24,37,25)','S4(24,38,24)','S5(23,38,26)']
 
-output=['S0001(25,41,25)','S0004(25,36,29)','S0005(26,31,29)','S0006(23,38,23)','S0007(66,38,29)',\
-        'S0009(25,39,24)','S0010(25,38,25)','S0012(25,41,26)','S0014(24,35,22)','S0016(22,35,24)',\
-        'S0020(27,29,28)','S0025(68,36,24)','S0027(27,30,26)','S0034(23,37,24)','S0035(26,38,25)',\
-        'S0036(24,38,27)','S0037(22,31,26)','S0038(23,34,26)','S0040(24,37,24)','S0041(24,37,25)',\
-        'S0051(22,38,27)','S0053(25,36,29)','S0057(25,40,24)','S0059(24,38,24)','S0068(21,38,24)',\
-        'S0069(24,29,28)','S0070(26,40,26)','S0074(24,39,26)','S0076(23,38,26)','S0077(21,40,22)',\
-        'S0081(23,38,26)','S0085(23,39,23)','S0087(25,35,25)','S0091(23,38,30)','S0092(65,38,24)']
-
-offsets  = [[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],\
-           [0,0,-1],[0,0,1],[1,1,0],[1,1,1],\
-           [1,1,-1],[0,1,1],[-1,1,1],[1,0,1],\
-           [1,-1,1],[-1,-1,0],[-1,-1,-1],[-1,-1,1],\
-           [0,-1,-1],[1,-1,-1],[-1,0,-1],[-1,1,-1],\
-           [0,1,-1],[0,-1,1],[1,0,-1],[1,-1,0],\
-           [-1,0,1],[-1,1,0]]
-
+offsets = [[1,0,0],[-1,0,0],\
+          [0,1,0],[0,-1,0],\
+          [0,0,-1],[0,0,1],\
+          [1,1,0],[1,1,1],\
+          [1,1,-1],[0,1,1],\
+          [-1,1,1],[1,0,1],\
+          [1,-1,1],[-1,-1,0],\
+          [-1,-1,-1],[-1,-1,1],\
+          [0,-1,-1],[1,-1,-1],\
+          [-1,0,-1],[-1,1,-1],\
+          [0,1,-1],[0,-1,1],\
+          [1,0,-1],[1,-1,0],\
+          [-1,0,1],[-1,1,0]]
 
 def inner_boundary(flag_image,inner_region_cor):
     """
@@ -187,7 +149,7 @@ for i in range(0,35):
     #nib.save(t_img,output+".nii.gz")
     print num
     x1 = range(1,num+1)
-    plt.plot(x1,y1,label='average contrast')
+    plt.plot(x1,y1,label='peripheral contrast')
 
     plt.show()
 
