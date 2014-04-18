c7cadb7283c49615cbfc1ee8f5b3368b573ccccc
liuzhaoguo <liu_zhaoguo@126.com>
2014-04-18 22:29:40 +0800

    add 2d and 3d region grow

--- group_data_analysis.py
+++ group_data_analysis.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 N=1000
 output=['S1(26,38,25)','S2(24,37,24)','S3(24,37,25)','S4(24,38,24)','S5(23,38,26)']
 
-result = np.zeros((N,36))
-for i in range(0,35):
+result = np.zeros((N,6))
+for i in range(0,5):
     j=0
     with open(output[i],'r') as f:
         for line in f:
@@ -14,9 +14,10 @@ for i in range(0,35):
             result[j][i] = map(float,line.split(','))[0]
             j=j+1
 for k in range(N):
-    result[k,35] = np.mean(result[k,0:35])
-plt.plot(result[:,35])
-plt.ylabel('average contrast')
+    result[k,5] = np.mean(result[k,0:5])
+plt.plot(result[:,5])
+plt.xlabel('voxels')
+plt.ylabel('group average of average contrast')
 plt.show()
 
 
