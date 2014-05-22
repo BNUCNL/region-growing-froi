8b0fa242f4307d189b26fb4b3d08761dfcabd8e8
zhougf0908@gmail.com <zhougf0908@gmail.com>
2014-05-22 16:12:05 +0800

    Add 4D and 3D test data.

--- tests/srgtest.py
+++ tests/srgtest.py
@@ -6,6 +6,11 @@ from algorithm.regiongrowing import *
 
 if __name__ == "__main__":
     print os.getcwd()
+
+    img4d = nib.load("../data/4DVolume.nii.gz")
+    affine4d = img4d.get_affine()
+    target_image_4d = img4d.get_data()
+
     img3d = nib.load("../data/S2/tstat1.nii.gz")
     affine3d = img3d.get_affine()
     target_image_3d = img3d.get_data()
@@ -20,17 +25,22 @@ if __name__ == "__main__":
     stop_criteria = StopCriteria(name='region_size', threshold=300)
     connectivity = Connectivity('6')
 
-    srg = SeededRegionGrowing(target_image_3d, seeds3d, similarity_criteria, stop_criteria, connectivity)
-    output3d = srg.grow()
-    nib.save(nib.Nifti1Image(output3d, affine3d), "../tests/SeededRegionGrowing3D.nii.gz")
+    srg = SeededRegionGrowing(target_image_4d, seeds3d, similarity_criteria, stop_criteria, connectivity)
+    output4d = srg.grow().get_current_region()
+    nib.save(nib.Nifti1Image(output4d.astype(int), affine3d), "../tests/SeededRegionGrowing4D.nii.gz")
+    print 'SeededRegionGrowing4D.nii.gz was created.'
+
+    srg.set_target_image(target_image_3d)
+    output3d = srg.grow().get_current_region()
+    nib.save(nib.Nifti1Image(output3d.astype(int), affine3d), "../tests/SeededRegionGrowing3D.nii.gz")
     print 'SeededRegionGrowing3D.nii.gz was created.'
 
     srg.get_connectivity().set_name('8')
     seeds2d = Seeds(np.array([24, 36]))
     srg.set_seeds(seeds2d)
-    srg.target_image = target_image_2d
-    output2d = srg.grow()
-    nib.save(nib.Nifti1Image(output2d, affine2d), "../tests/SeededRegionGrowing2D.nii.gz")
+    srg.set_target_image(target_image_2d)
+    output2d = srg.grow().get_current_region()
+    nib.save(nib.Nifti1Image(output2d.astype(int), affine2d), "../tests/SeededRegionGrowing2D.nii.gz")
 
     print 'SeededRegionGrowing2D.nii.gz was created.'
     print '------------------END------------------'
