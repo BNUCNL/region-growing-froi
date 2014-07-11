e2d4050467c4d9bd3c61d42e3446600a8661e6f2
zhenzonglei <zhenzonglei@gmail.com>
2014-07-11 13:21:26 +0800

    refactor region growing module -- version 2

--- algorithm/regiongrowing.py
+++ algorithm/regiongrowing.py
@@ -4,27 +4,29 @@ import numpy as np
 import nibabel as nib
 from scipy.spatial import distance
 
+import utils
+
 
 class SeededRegionGrowing:
     """
     Seeded region growing with a fixed threshold.
     """
 
-    def __init__(self, target_image, seeds, similarity_criteria, stop_criteria, neighbor):
+    def __init__(self, image, seeds, similarity_criteria, stop_criteria, neighbor):
         """
         Parameters
         -----------------------------------------------------
-        target_image: input image, a 2D/3D/4D Nifti1Image object or N-D array
+        image: input image, a 2D/3D/4D Nifti1Image object or N-D array
         seeds: an instance of class Seed
         similarity_criteria: an instance of class SimilarityCriteria
         stop_criteria: an instance of class StopCriteria
         neighbor: an instance of class Connectivity(SpatialNeighbor)
         """
 
-        if isinstance(target_image, nib.nifti1.Nifti1Image):
-            target_image = target_image.get_data()
+        if isinstance(image, nib.nifti1.Nifti1Image):
+            target_image = image.get_data()
 
-        if 2 <= len(target_image.shape) <= 4:
+        if 2 <= len(image.shape) <= 4:
             self.image = target_image
         else:
             raise ValueError("Target image must be a 2D/3D/4D.")
@@ -43,8 +45,8 @@ class SeededRegionGrowing:
         self.region = Region(region_label, region_neighbor)
 
 
-    def set_image(self, target_image):
-        self.image = target_image
+    def set_image(self, image):
+        self.image = image
 
     def get_image(self):
         return self.image
@@ -83,13 +85,13 @@ class SeededRegionGrowing:
         """
         Set the similarity criteria.
         """
-        self.set_similarity_criteria(similarity_criteria)
+        self.similarity_criteria = similarity_criteria
 
     def get_similarity_criteria(self):
         """
         Get the similarity criteria.
         """
-        return self.get_similarity_criteria()
+        return self.similarity_criteria
 
     def set_region(self, region):
         """
@@ -97,7 +99,7 @@ class SeededRegionGrowing:
         """
         self.region = region
 
-    def get_region_sequence(self):
+    def get_region(self):
         """
         Get the region sequence..
         """
@@ -110,11 +112,13 @@ class SeededRegionGrowing:
         """
 
         while not self.stop_criteria.isstop():
-            nearest_neighbor = self.similarity_criteria.computing(self.region, self.image)
+            nearest_neighbor = self.similarity_criteria.compute(self.region, self.image)
             self.region.add_label(nearest_neighbor)
-            self.neighbor.add_neighbor(self.neighbor(nearest_neighbor))
 
-            self.stop_criteria.computing(self.region, self.image)
+            self.region.remove_neighbor(nearest_neighbor)
+            self.region.add_neighbor(self.neighbor.compute(nearest_neighbor))
+
+            self.stop_criteria.compute(self.region, self.image)
         return self.region
 
 
@@ -134,7 +138,7 @@ class Seeds(object):
 
         self.coords = coords
 
-    def generating(self):
+    def generate(self):
         """
         Generating new seeds.       """
         return self.coords
@@ -159,7 +163,7 @@ class RandomSeeds(Seeds):
         else:
             self.sampling_number = sampling_number
 
-    def generating(self):
+    def generate(self):
         """
         Generating new seeds.
 
@@ -178,7 +182,7 @@ class RandomSeeds(Seeds):
 
 class SimilarityCriteria:
     """
-    Similarity criteria: compute the similairity between region and neighbor voxels
+    Similarity criteria: compute the similarity between region and neighbor voxels
     """
 
     def __init__(self, metric='educlidean'):
@@ -204,21 +208,25 @@ class SimilarityCriteria:
         """
         return self.metric
 
-    def computing(self, region, img=None, mask_img=None, prior_img=None):
+    def compute(self, region, image=None, mask_image=None, prior_image=None):
         """
-        Compute the  similarity.
+        Compute the similarity between the labeled region and neighbors.
+
         Parameters
         -----------------------------------------------------
-        region:The region growing.
-        raw_image: The raw image.
-        mask_image: the mask image may be used in the compute process. which should be a ndarray type.
-        prior_image:the prior image may be used in the compute process. which should be a ndarray type.
+        region: the labeled region, an instance of class Region.
+        image: The raw image where the region grow from.
+        mask_image: the mask image may be used in the compute process. which should be a ndarray.
+        prior_image:the prior image may be used in the compute process. which should be a ndarray.
         """
-
-        region_mean = np.mean(img[region.extent, :])
-        neighbor_intensity = img(region.neighbor)
-        dist = distance.cdist(region_mean, neighbor_intensity, self.metric)
+        lsize = region.label_size
+        nsize = region.neighbor_size
+        region_val = np.mean(image[region.label[:lsize, 0], region.label[:lsize, 1], region.label[:lsize, 2], :])
+        neighbor_val = image[region.neighbor[:nsize, 0], region.neighbor[:nsize, 1], region.neighbor[:nsize, 2], :]
+        dist = distance.cdist(region_val, neighbor_val, self.metric)
         index = dist.argmin()
+
+        return region.neighbor[index, :]
 
 
 class StopCriteria(object):
@@ -236,15 +244,15 @@ class StopCriteria(object):
         """
 
         self.type = criterion_type
-        self.name = criterion_metric
+        self.metric = criterion_metric
         self.threshold = threshold
         self.stop = False
 
-    def set_metric(self, metric_name):
+    def set_metric(self, metric):
         """
         Get the name of the stop criteria..
         """
-        self.metric = metric_name
+        self.metric = metric
 
     def get_metric(self):
         """
@@ -253,14 +261,16 @@ class StopCriteria(object):
         return self.metric
 
 
-    def computing(self, region, raw_image=None, mask_image=None, prior_image=None):
+    def compute(self, region, raw_image=None, mask_image=None, prior_image=None):
         """
-        Set the stop criteria.
+
+        check whether growing should stop according to the region and image
+
         """
-        if cmp(self.type, 'morphology'):
-            if cmp(self.metric, 'region_size'):
-                region_size = np.count_nonzero(region.extent)
-                if region_size >= self.threshold:
+
+        if self.type == 'morphology':
+            if self.metric == 'region_size':
+                if region.shape[0] >= self.threshold:
                     self.stop = True
 
     def isstop(self):
@@ -325,14 +335,21 @@ class Region(object):
         self.label[self.label_size:(self.label_size + new_label.shape[0]), :] = new_label
         self.label_size = self.label_size + new_label.shape[0]
 
+
     def add_neighbor(self, new_neighbor):
 
-        self.label[self.neighbor_size:(self.neighbor_size + new_neighbor.shape[0]), :] = new_neighbor
+        marked = utils.in2d(new_neighbor, self.neighbor[:self.neighbor_size, :]) \
+            or utils.in2d(new_neighbor, self.label[:, self.label_size, :])
+        np.delete(new_neighbor, marked, 0)
+
+        self.neighbor[self.neighbor_size:(self.neighbor_size + new_neighbor.shape[0]), :] = new_neighbor
         self.neighbor_size = self.neighbor_size + new_neighbor.shape[0]
 
-        for point in points:
-            if inside(point, image_shape) and not region_neighbor[tuple(point)] and not cur_region[tuple(point)]:
-                region_neighbor[tuple(point)] = True
+    def remove_neighbor(self, new_label):
+
+        idx = np.nonzero(utils.in2d(self.neighbor[:self.neighbor_size, :], new_label))
+        np.delete(self.neighbor, idx, 0)
+        self.neighbor_size = self.neighbor_size - idx.shape[0]
 
 
 def compute_inner_boundary(self):
@@ -351,12 +368,12 @@ def compute_external_boundary(self):
         pass
 
 
-class RegionOptimize(Region):
+class Optimizer(object):
     """
     Region optimizer.
     """
 
-    def __init__(self, target_image, seeds, upperlimit, connectivity):
+    def __init__(self, image, region):
         """
         Parameters
         -----------------------------------------------------
@@ -367,12 +384,12 @@ class RegionOptimize(Region):
         """
 
 
-class Aggregator:
+class Aggregator(object):
     """
     Seeded region growing based on random seeds.
     """
 
-    def __init__(self, seeds, region_sequence, raw_image, aggregator_type='average'):
+    def __init__(self, image, region, type='average'):
         """
         Parameters
         -----------------------------------------------------
@@ -380,72 +397,36 @@ class Aggregator:
         raw_image: raw image.
         aggregator_type: 'average', 'magnitude', 'homogeneity', default is 'average'.
         """
-        if not isinstance(region_sequence, np.ndarray):
-            raise ValueError("The input region sequence  must be ndarray type. ")
 
-        if not isinstance(raw_image, np.ndarray):
-            raise ValueError("The input raw_image  must be ndarray type. ")
+        pass
 
-        if not isinstance(aggregator_type, str):
-            raise ValueError("The value of aggregator_type must be str type. ")
 
-    def set_seeds(self, seeds):
-        self.seeds = seeds
-
-    def get_seeds(self):
-        return self.seeds
-
-    def aggregator(self):
+    def compute(self, ):
         """
         Aggregation for different regions
         """
 
 
-class RandomSRG:
+class RandomSRG(SeededRegionGrowing):
     """
     Seeded region growing based on random seeds.
     """
 
-    def __init__(self, n_seeds, stop_criteria, ):
+    def __init__(self, image, seeds, similarity_criteria, stop_criteria, neighbor, aggregator):
         """
         Parameters
         -----------------------------------------------------
         n_seeds: n seeds.
         stop_criteria: stop criteria about the n regions from n seeds.
         """
-        if not isinstance(n_seeds, list):
-            raise ValueError("The input seeds  must be list type. ")
-        else:
-            self.set_seeds(n_seeds)
-        self.aggregator()
+        super(RandomSRG, self).__init__(image, seeds, similarity_criteria, stop_criteria, neighbor)
+        self.aggregator = aggregator
 
 
-    def set_seeds(self, seeds):
-        self.seeds = seeds
-
-    def get_seeds(self):
-        return self.seeds
-
-    def set_stop_criteria(self, region, stop_type, stop_criteria):
-        """
-        Set the stop criteria.
-        """
-        self.stop_criteria = StopCriteria(region, stop_type, stop_criteria)
-
-    def get_stop_criteria(self):
-        """
-        Return the stop criteria.
-        """
-        return self.stop_criteria
-
-    def aggregator(self):
+    def grow(self):
         """
         Aggregation for different regions
         """
-        n_regions = None
-        raw_image = None
-        self.aggregator = Aggregator(self.seeds, n_regions, raw_image)
-        self.aggregator.aggregator()
 
 
 class AdaptiveSRG(SeededRegionGrowing):
@@ -453,31 +434,14 @@ class AdaptiveSRG(SeededRegionGrowing):
     Adaptive seeded region growing.
     """
 
-    def __init__(self, target_image, seeds, upperlimit, connectivity):
-        if not isinstance(seeds, np.ndarray):
-            seeds = np.array(seeds)
-        self.target_image = target_image
-        self.set_seeds(seeds)
-        self.connectivity = connectivity
-        self.get_uplimit = upperlimit
+    def __init__(self, image, seeds, similarity_criteria, stop_criteria, neighbor, ):
+        super(AdaptiveSRG, self).__init__(image, seeds, similarity_criteria, stop_criteria, neighbor)
 
-    def growing(self):
+
+    def grow(self):
         """
         Adaptive region growing.
         """
-        image = self.target_image
-        seeds = Seeds(self.seeds)
-        similarity_criteria = NeighborSimilarity(metric='euclidean')
-        connectivity = Connectivity('26')
-
-        region_sequence = []
-        child_vector = self.stop_criteria.computing
-        for i in child_vector:
-            stop_criteria_temp = StopCriteria(name='region_size', threshold=i)
-            self.set_stop_criteria(stop_criteria_temp)
-            region = self.grow()
-            region_sequence.append(region)
-        return region_sequence
 
 
 if __name__ == "__main__":
